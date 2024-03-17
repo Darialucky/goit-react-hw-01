a1deba96348e7febaedd161f2ece36da9768a5f4
Darialucky <144655337+Darialucky@users.noreply.github.com>
2024-03-17 17:39:15 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,6 @@ export default function App() {
   return (
     <div>
       <h1>Best selling</h1>
-
       <Product
         name="Tacos With Lime"
         imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
