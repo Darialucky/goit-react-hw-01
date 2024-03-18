cc09d425ec768bee1d1fe1673d1e080aeda1474d
Darialucky <144655337+Darialucky@users.noreply.github.com>
2024-03-18 17:02:08 +0200

    Friends

--- src/components/FriendsList/FriendList.jsx
+++ src/components/FriendsList/FriendList.jsx
@@ -2,12 +2,15 @@ import FriendListItem from "../FriendListItem/FriendListItem";
 import styles from "./FriendList.module.css";
 
 const FrendList = ({ friends }) => (
-  <ul className={styles.friendlist}>
-    {friends.map(({ avatar, name, isOnline, id }) => (
-      <li key={id} className={styles.friendCard}>
-        <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
-      </li>
-    ))}
-  </ul>
+  <div>
+    <h2>FRIENDS</h2>
+    <ul className={styles.friendlist}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <li key={id} className={styles.friendCard}>
+          <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+        </li>
+      ))}
+    </ul>
+  </div>
 );
 export default FrendList;
